c7b47a206e7924556cb6fac3fb331e8cc16f562a
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-04 23:45:01 +0200

    Feat: Add Footer and Navbar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import CircularSliderSection from './components/carousel/CircularSliderSection';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <CircularSliderSection />
+      <Footer />
     </div>
   );
 }
