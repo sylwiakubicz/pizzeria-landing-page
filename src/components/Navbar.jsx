cacfd528d9b4de9320f4b2ee75c71801bcdd6680
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-06 14:15:10 +0200

    Feat: Add navbar elements with some styling.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import "../styles/navbar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faPhone } from '@fortawesome/free-solid-svg-icons'
 
-function Navbar() {
+const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav>
+            <h2 className='logo'>Lorem, ipsum.</h2>
+            <ul className='navList'>
+                <li>
+                    <a>Home</a>
+                </li>
+                <li>
+                    <a>Menu</a>
+                </li>
+                <li>
+                    <a>Special & Coupons</a>
+                </li>
+                <li>
+                    <a>Find us</a>
+                </li>
+            </ul>
+            <ul className='navIcons'>
+                <li>
+                    <FontAwesomeIcon icon={faCartShopping} />
+                </li>
+                <li>
+                    <FontAwesomeIcon icon={faPhone} />
+                </li>
+            </ul>
+        </nav>
     )
 }
 
