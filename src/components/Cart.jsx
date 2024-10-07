88122de363f40d35280c5abfe7a8c0f4cc84fe7d
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-07 22:47:28 +0200

    Feat: Add elements to Cart component.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Cart = () => {
     return (
-    <div>Cart</div>
+    <div className='cart'>
+        <h2>My order</h2>
+        <div>
+            <h3>Cart is empty</h3>
+            <p>Add a dish to continue</p>
+        </div>
+        <span>
+            <p>Order</p>
+            <p>0.00$</p>
+        </span>
+    </div>
     )
 }
 
