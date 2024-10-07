01d3bf7b645a51dfc6e3d033e4b5eff25fa31b2c
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-07 21:13:45 +0200

    Feat: Create SideBar component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import CircularSliderSection from './components/carousel/CircularSliderSection';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
     <div>
       <Navbar />
       <CircularSliderSection />
+      <SideBar />
       <Footer />
     </div>
   );
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/sideBar.css'
+
+const SideBar = () => {
+    return (
+        <div className='sideBarContainer'>
+            
+        </div>
+    )
+}
+
+export default SideBar 
