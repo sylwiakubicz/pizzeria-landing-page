211d020ed8ac892ad4bd824c2b6d2fc63a50abe8
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-07 23:15:45 +0200

    Style: Add styling to Cart component.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Cart = () => {
     return (
-    <div className='cart'>
-        <h2>My order</h2>
-        <div>
+    <div className='wraper'>
+        <h2 className='sidebarHeader'>My order</h2>
+        <div className='emptyCart'>
+            <FontAwesomeIcon icon={faCartShopping} className='cartIcon'/>
             <h3>Cart is empty</h3>
             <p>Add a dish to continue</p>
         </div>
-        <span>
+        <div className='costSummary'>
             <p>Order</p>
-            <p>0.00$</p>
-        </span>
+            <p>0.00 $</p>
+        </div>
     </div>
     )
 }
