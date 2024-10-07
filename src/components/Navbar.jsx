01da72906df8dfdcc0b60116e77586c5447e8d9b
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-07 20:11:28 +0200

    Feat: Show navbar when scroll up. Hidden when scrolling down.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import "../styles/navbar.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping, faPhone } from '@fortawesome/free-solid-svg-icons'
+import React, { useState, useEffect } from 'react';
+import "../styles/navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+    const [showNavbar, setShowNavbar] = useState(true)
+    const [lastScrollY, setLastScrollY] = useState(0)
+
+    const handleScroll = () => {
+        if (window.scrollY > lastScrollY) {
+            setShowNavbar(false);
+        } else {
+            setShowNavbar(true);
+        }
+        setLastScrollY(window.scrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [lastScrollY]);
+
     return (
-        <nav>
+        <nav className={`navbar ${showNavbar ? 'visible' : 'hidden'}`}>
             <h2 className='logo'>Lorem, ipsum.</h2>
             <ul className='navList'>
                 <li>
@@ -30,7 +49,7 @@ const Navbar = () => {
                 </li>
             </ul>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
