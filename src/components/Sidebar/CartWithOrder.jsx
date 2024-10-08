c085143787c4fd926bdddca51cb42e04ea7d7695
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-08 19:06:35 +0200

    Feat: Create component CartWithOrder.

---
+++ src/components/Sidebar/CartWithOrder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartWithOrder = () => {
+    return (
+        <div>CartWithOrder</div>
+    )
+}
+
+export default CartWithOrder
--- src/components/Sidebar/EmptyCart.jsx
+++ src/components/Sidebar/EmptyCart.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function EmptyCart() {
+const EmptyCart = () => {
     return (
         <div className='emptyCart'>
             <FontAwesomeIcon icon={faCartShopping} className='cartIcon'/>
