f5fc5efd560dfefb052920f17053b639ccd501a9
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-11 22:09:30 +0200

    Feat: Add menuSlice reducer to store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import toggleSidebarReducer from "./toggleSidebar/toggleSidebarSlice"
+import menuReducer from "./menu/menuSlice"
 
 
 export const store = configureStore({
     reducer: {
-        toggleSidebar: toggleSidebarReducer
+        toggleSidebar: toggleSidebarReducer,
+        menu: menuReducer,
     }
 });
 
