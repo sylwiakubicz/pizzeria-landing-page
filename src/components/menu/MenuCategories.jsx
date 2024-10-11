e481ad6e20bcf1fc6d1476325c00b905a953b145
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-11 20:13:27 +0200

    Feat: Create MenuCategories component.

---
+++ src/components/menu/MenuCategories.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MenuCategories() {
+    return (
+        <div className='menuCategories'>
+            <button className='active'>Pizzas</button>
+            <button>Drinks</button>
+            <button>Additionals</button>
+        </div>
+    )
+}
+
+export default MenuCategories
