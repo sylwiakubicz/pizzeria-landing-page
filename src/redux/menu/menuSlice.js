dafc0dc1738cef2efdc3b520e5d40960279467b2
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-11 21:02:09 +0200

    Feat: Create menuSlice.

---
+++ src/redux/menu/menuSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const menuSlice = createSlice({
+    name: 'menu',
+    initialState: {
+        selectedCategory: 'pizzas',
+        menuData: null
+    },
+    reducers: {
+        setCategory: (state, action) => {
+            state.selectedCategory = action.payload;
+        },
+        setMenuData: (state, action) => {
+            state.menuData = action.payload;
+        }
+    }
+})
+
+export const {} = menuSlice.actions;
+
+export const selectCategory = (state) => state.menu.selectedCategory;
+export const selectMenuData = (state) => state.menu.menuData;
+
+export default menuSlice.reducer;
