57366473da18efda7cc28f5a7f93489d54c53301
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-11 22:10:49 +0200

    Feat: integrate Redux for category selection

--- src/components/menu/MenuCategories.jsx
+++ src/components/menu/MenuCategories.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import {setCategory, selectCategory } from '../../redux/menu/menuSlice'
 
 function MenuCategories() {
+
+    const dispatch = useDispatch();
+    const selectedCategory = useSelector(selectCategory);
+
+    const handleCategoryClick = (category) => {
+        dispatch(setCategory(category));
+    };
+
     return (
         <div className='menuCategories'>
-            <button className='active'>Pizzas</button>
-            <button>Drinks</button>
-            <button>Additionals</button>
+            <button 
+                className={selectedCategory === 'pizzas' ? 'active' : ''}
+                onClick={() => handleCategoryClick('pizzas')}>
+                    Pizzas
+            </button>
+            <button 
+                className={selectedCategory === 'drinks' ? 'active' : ''}
+                onClick={() => handleCategoryClick('drinks')}>
+                    Drinks
+            </button>
+            <button 
+                className={selectedCategory === 'additionals' ? 'active' : ''}
+                onClick={() => handleCategoryClick('additionals')}>
+                    Additionals
+            </button>
         </div>
     )
 }
