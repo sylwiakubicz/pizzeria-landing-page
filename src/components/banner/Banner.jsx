1cb564401806462696a2f45edbcb1f42008a3426
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-12 21:51:36 +0200

    Refactor: Design banner.

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,7 +4,13 @@ import "./banner.css"
 const Banner = () => {
     return (
         <section id="home" className='banner'>
-            {/* <img src="/images/banner.jpeg"/> */}
+            <img src="/images/delicious-pizza-indoors.png"/>
+            <div className='bannerText'>
+                <h1>Lorem ipsum <span className='color'>dolor sit, amet</span> consectetur.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab esse voluptatum nisi, labore ad blanditiis eveniet maiores dicta, perspiciatis a facilis exercitationem velit! Voluptatibus, exercitationem.</p>
+                <button>Order now</button>
+            </div>
+            <img className='border' src="/images/wooden-bg.png"/> 
         </section>
     )
 }
