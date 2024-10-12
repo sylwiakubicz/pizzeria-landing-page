40de6ed80aab6fbe0ca189dc011949148b71c393
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-12 19:51:01 +0200

    Feat: Add PizzaDesc compoonent to carosuel.

--- src/components/carousel/CircularSliderSection.jsx
+++ src/components/carousel/CircularSliderSection.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CircularSliderWraper from './CircularSliderWraper'
 import CircularSliderControlsWraper from './CircularSliderControlsWraper'
 import './circularSlider.css'
 import { useCarousel } from '../../hooks/useCarousel';
+import PizzaDesc from './PizzaDesc';
 
 
 const CircularSliderSection = () => {
 
   const { rotate, j, handleMauseEnter, handleMauseLeave, handleControlClick } = useCarousel(8); 
-
+  useEffect(() => {
+    console.log("current index: " + j.current)
+  }, [j.current])
   return (
     <section className='slideSection' id="pizzas" aria-labelledby="menu-title">
       <h2 id="menu-title" className="visually-hidden">Menu</h2>
@@ -18,6 +21,7 @@ const CircularSliderSection = () => {
         handleMauseEnter={handleMauseEnter} 
         handleMauseLeave={handleMauseLeave} 
         aria-label="Circular slider showcasing menu items"/>
+        <PizzaDesc currentPizzaIndex={j.current - 1}/>
       <CircularSliderControlsWraper 
         activeIndex={j.current} 
         handleControlClick={handleControlClick} 
---
+++ src/components/carousel/PizzaDesc.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useCarouselData } from '../../hooks/useCarouselData';
+
+function PizzaDesc({currentPizzaIndex}) {
+
+    const menuData = useCarouselData()
+
+    return (
+        <div className='pizzaDesc'>
+            <h3>{menuData.carouselData[currentPizzaIndex].name}</h3>
+            <p>{menuData.carouselData[currentPizzaIndex].ingredients.join(', ')}</p>    
+        </div>
+    )
+}
+
+export default PizzaDesc
