dd8736dd3ac1b6c6039056d984fc5fdbf96c6e30
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-13 12:19:23 +0200

    Feat: Order now button opens contact sidebar.

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import "./banner.css"
+import { useDispatch } from 'react-redux';
+import {toggleSidebarAction} from '../../redux/toggleSidebar/toggleSidebarSlice';
 
 const Banner = () => {
+
+    const dispatch = useDispatch();
+
     return (
         <section id="home" className='banner'>
             <img src="/images/delicious-pizza-indoors.png"/>
             <div className='bannerText'>
                 <h1>Lorem ipsum <span className='gradientText'>dolor sit, amet</span> consectetur.</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab esse voluptatum nisi, labore ad blanditiis eveniet maiores dicta, perspiciatis a facilis exercitationem velit! Voluptatibus, exercitationem.</p>
-                <button>Order now</button>
+                <button onClick={() => dispatch(toggleSidebarAction('contact'))}>Order now</button>
             </div>
         </section>
     )
