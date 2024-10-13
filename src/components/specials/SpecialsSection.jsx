ff5aefe861df6cd36bac34214803deb477c58373
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-13 19:03:23 +0200

    Feat: Add elements and style to specials section.

--- src/components/specials/SpecialsSection.jsx
+++ src/components/specials/SpecialsSection.jsx
@@ -4,7 +4,8 @@ import "./specialsSection.css"
 const SpecialsSection = () => {
     return (
         <section className='special' id="special">
-            <h2>special</h2>
+            <h2 className='gradientText'>Specials & Coupons</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum necessitatibus dolore error. Aliquam non exercitationem delectus totam, harum quisquam soluta ab aliquid officia vitae? Cumque.</p>
         </section>
     )
 }
