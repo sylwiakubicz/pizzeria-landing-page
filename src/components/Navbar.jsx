b464845a07e1f1d140b84f52aeb42b0bbfe6efcb
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-15 19:07:29 +0200

    Feat: Add toggle button and dropdown menu to navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ import { useDispatch } from 'react-redux';
 import {toggleSidebarAction} from '../redux/toggleSidebar/toggleSidebarSlice';
 import { useScrollDirection } from '../hooks/useScrollDirection';
 import { useActiveSection } from '../hooks/useActiveSection';
-import { faSquareFull } from "@fortawesome/free-solid-svg-icons";
+import { faSquareFull, faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Navbar = () => {
 
@@ -13,9 +14,13 @@ const Navbar = () => {
     const showNavbar = useScrollDirection();
     const activeSection = useActiveSection();  
 
+    const [btnActive, setBtnActive] = useState(false)
+
     return (
 <nav className={`navbar ${showNavbar ? 'visible' : 'hidden'}`}>
             <h2 className='logo'>Lorem, ipsum.</h2>
+
+            {/* screen width > 1024px */}
             <ul className='navList'>
                 <li>
                     <a href='#home' className={activeSection === 'home' ? 'gradientText' : ''} aria-label="Go to Home section">Home</a>
@@ -33,6 +38,33 @@ const Navbar = () => {
                     <a href='#map' className={activeSection === 'map' ? 'gradientText' : ''} aria-label="Go to Find Us section">Find us</a>
                 </li>
             </ul>
+
+            {/* screen width < 1024px */}
+
+            <div className="btnToggle" onClick={() => setBtnActive(prev => !prev)}>
+                <FontAwesomeIcon icon={faBars}/>
+            </div>
+
+            <div className="dropdownMenu">
+            <ul className='navDropdownList' style={btnActive ? { right: 0 } : {right: -500}}>
+                <li>
+                    <a href='#home' className={activeSection === 'home' ? 'gradientText' : ''} aria-label="Go to Home section">Home</a>
+                </li>
+                <li>
+                    <a href='#pizzas' className={activeSection === 'pizzas' ? 'gradientText' : ''} aria-label="Go to pizzas section">Our pizzas</a>
+                </li>
+                <li>
+                    <a href='#menu' className={activeSection === 'menu' ? 'gradientText' : ''} aria-label="Go to Menu section">Menu</a>
+                </li>
+                <li>
+                    <a href='#special' className={activeSection === 'special' ? 'gradientText' : ''} aria-label="Go to Special & Coupons section">Special & Coupons</a>
+                </li>
+                <li>
+                    <a href='#map' className={activeSection === 'map' ? 'gradientText' : ''} aria-label="Go to Find Us section">Find us</a>
+                </li>
+            </ul>
+            </div>
+
             <ul className='navIcons'>
                 <li>
                     <FontAwesomeIcon 
