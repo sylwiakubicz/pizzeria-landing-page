d949f5e2b9cce6e20998d242343c0549b1a133ae
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-17 20:48:59 +0200

    Refactor: Create one ofert basic layout - to change.

--- src/components/specials/SpecialsSection.jsx
+++ src/components/specials/SpecialsSection.jsx
@@ -8,7 +8,15 @@ const SpecialsSection = () => {
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum necessitatibus dolore error. Aliquam non exercitationem delectus totam, harum quisquam soluta ab aliquid officia vitae? Cumque.</p>
             <div className='ofertsWrapper'>
                 <div className='ofertWraper'></div>
-                <div className='ofertWraper'></div>
+                <div className='ofertWraper'>
+                    <h2 className='gradientText'>Pizza "Half Half"</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, fugiat.</p>
+                    <div className='halfPizzaWraper'>
+                        <div className='halfPizza halfPizzaLeft'></div>
+                        <div className='dividePizzas'></div>
+                        <div className='halfPizza halfPizzaRight'></div>
+                    </div>
+                </div>
                 <div className='ofertWraper'></div>
             </div>        
         </section>
