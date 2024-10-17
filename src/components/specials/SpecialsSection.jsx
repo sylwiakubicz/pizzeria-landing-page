e0c4c65faddc436f72a53eaee54c8d554d1b42c0
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-10-17 20:18:17 +0200

    Refacotr: Create containers for specials and coupons.

--- src/components/specials/SpecialsSection.jsx
+++ src/components/specials/SpecialsSection.jsx
@@ -6,6 +6,11 @@ const SpecialsSection = () => {
         <section className='special' id="special">
             <h2 className='gradientText'>Specials & Coupons</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum necessitatibus dolore error. Aliquam non exercitationem delectus totam, harum quisquam soluta ab aliquid officia vitae? Cumque.</p>
+            <div className='ofertsWrapper'>
+                <div className='ofertWraper'></div>
+                <div className='ofertWraper'></div>
+                <div className='ofertWraper'></div>
+            </div>        
         </section>
     )
 }
